ca1bb5a5d4e171c5df5b8876395e092cd83fab70
Mark Anthony <markanthonymartinez9@gmail.com>
2018-05-30 07:47:54 -0500

    Create tag appender function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
+//List of Variables
+const listOfCards = document.querySelectorAll('.card');
+
 //this function when called creates img elements
 function makeImgTag() {
   const imgEle = document.createElement('img');
   return imgEle;
+}
+
+//This function will append created tags to elements
+function appendToElement(array) {
+  for (let i = 0; i < array.length; i++) {
+    let tagContainer = makeImgTag();
+    array[i].appendChild(tagContainer);
+  }
 }
