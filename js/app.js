b0734f99b96d02d74c972324a10b88f36ffb1be2
Mark Anthony <markanthonymartinez9@gmail.com>
2018-05-31 00:33:53 -0500

    Add image array

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 //List of Variables
 const listOfCards = document.querySelectorAll('.card');
 
+//Array used to store card images
+let imgArray = ['https://avatarfiles.alphacoders.com/242/24282.jpg', 'https://avatarfiles.alphacoders.com/241/24193.jpg', 'https://avatarfiles.alphacoders.com/235/23542.jpg', 'https://avatarfiles.alphacoders.com/226/22680.jpg', 'https://avatarfiles.alphacoders.com/187/18787.jpg', 'https://avatarfiles.alphacoders.com/481/4816.jpg', 'https://avatarfiles.alphacoders.com/471/4717.jpg', 'https://avatarfiles.alphacoders.com/799/79.jpg', 'https://avatarfiles.alphacoders.com/114/114197.jpg', 'https://avatarfiles.alphacoders.com/841/84143.png', 'https://avatarfiles.alphacoders.com/583/58365.jpg', 'https://avatarfiles.alphacoders.com/253/25343.jpg']
+
 //this function when called creates img elements
 function makeTag(string) {
   const imgEle = document.createElement(string);
