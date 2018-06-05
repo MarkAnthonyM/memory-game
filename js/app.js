85cc35112f10fdc663873c422d40dccc31381061
Mark Anthony <markanthonymartinez9@gmail.com>
2018-06-05 18:29:58 -0500

    Refactor cardFlip code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //List of Variables
 const listOfCards = document.querySelectorAll('.card');
 const listOfImgTags = document.getElementsByTagName('img');
+const mainTable = document.getElementById('cardTable');
 
 //Array used to store card images
 let imgArray = ['https://avatarfiles.alphacoders.com/242/24282.jpg', 'https://avatarfiles.alphacoders.com/241/24193.jpg', 'https://avatarfiles.alphacoders.com/235/23542.jpg', 'https://avatarfiles.alphacoders.com/226/22680.jpg', 'https://avatarfiles.alphacoders.com/187/18787.jpg', 'https://avatarfiles.alphacoders.com/481/4816.jpg', 'https://avatarfiles.alphacoders.com/471/4717.jpg', 'https://avatarfiles.alphacoders.com/799/79.jpg', 'https://avatarfiles.alphacoders.com/114/114197.jpg', 'https://avatarfiles.alphacoders.com/841/84143.png', 'https://avatarfiles.alphacoders.com/583/58365.jpg', 'https://avatarfiles.alphacoders.com/253/25343.jpg']
@@ -53,7 +54,7 @@ function addImgToCards(imagesArray, tagArray, attr) {
 function checkMatch(array) {
   const toRemove = document.getElementsByClassName('flipped')
   if (array[0] === array[1]) {
-    for (var i = 0; i < toRemove.length; i++) {
+    for (let i = 0; i < toRemove.length; i++) {
       toRemove[i].style.visibility = 'hidden';
     }
   } else {
@@ -63,34 +64,56 @@ function checkMatch(array) {
 
 //Function will reset card when called
 function resetCard(array) {
-  for (var i = 0; i < array.length; i++) {
-    array[i].style.display = 'none';
-    array[i].setAttribute('class', '');
+  while (array.length > 0) {
+    array[0].style.display = 'none'
+    array[0].setAttribute('class', '');
   }
 }
 
 //Code will flip card when clicked on and check for matching cards
-function cardFlip(array, tag) {
+function cardFlip() {
   tempArray = [];
-  for (let i = 0; i < array.length; i++) {
-    array[i].addEventListener('click', function(event) {
-      if (tag[i].style.display === 'none') {
-        tag[i].style.display = '';
-        tag[i].setAttribute('class', 'flipped');
-        tempArray.push(event.target.firstElementChild.currentSrc);
-        if (tempArray.length === 2) {
-          checkMatch(tempArray);
-          tempArray = [];
-        }
-      } else {
-        tag[i].style.display = 'none';
-        tag[i].setAttribute('class', '');
+  mainTable.addEventListener('click', function(event) {
+    if (event.target.getAttribute('class') === 'card') {
+      event.target.firstElementChild.style.display = '';
+      event.target.firstElementChild.setAttribute('class', 'flipped');
+      tempArray.push(event.target.firstElementChild.currentSrc);
+      if (tempArray.length === 2) {
+        setTimeout(checkMatch, 3000, tempArray);
+        tempArray = [];
+      }
+    } else {
+      if (event.target.nodeName === 'IMG') {
+        event.target.style.display = 'none';
+        event.target.setAttribute('class', '');
         tempArray.splice(0, 1);
       }
-    })
-  }
+    }
+  })
 }
+
+//Older, less efficent code. Keeping around for documentation purposes.
+// function cardFlip(array, tag) {
+//   tempArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     array[i].addEventListener('click', function(event) {
+//       if (tag[i].style.display === 'none') {
+//         tag[i].style.display = '';
+//         tag[i].setAttribute('class', 'flipped');
+//         tempArray.push(event.target.firstElementChild.currentSrc);
+//         if (tempArray.length === 2) {
+//           checkMatch(tempArray);
+//           tempArray = [];
+//         }
+//       } else {
+//         tag[i].style.display = 'none';
+//         tag[i].setAttribute('class', '');
+//         tempArray.splice(0, 1);
+//       }
+//     })
+//   }
+// }
 
 appendToElement(listOfCards, 'img');
 addImgToCards(imgArray, listOfImgTags, 'src');
-cardFlip(listOfCards, listOfImgTags);
+cardFlip();
