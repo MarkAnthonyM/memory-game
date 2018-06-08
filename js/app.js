95d56bb7e402e31c12781377fef39c92f277eeef
Mark Anthony <markanthonymartinez9@gmail.com>
2018-06-08 01:15:03 -0500

    Add gameWin function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const listOfCards = document.querySelectorAll('.card');
 const listOfImgTags = document.getElementsByTagName('img');
 const mainTable = document.getElementById('cardTable');
+const cardsMatched = [];
 
 //Array used to store card images
 let imgArray = ['https://avatarfiles.alphacoders.com/242/24282.jpg', 'https://avatarfiles.alphacoders.com/241/24193.jpg', 'https://avatarfiles.alphacoders.com/235/23542.jpg', 'https://avatarfiles.alphacoders.com/226/22680.jpg', 'https://avatarfiles.alphacoders.com/187/18787.jpg', 'https://avatarfiles.alphacoders.com/481/4816.jpg', 'https://avatarfiles.alphacoders.com/471/4717.jpg', 'https://avatarfiles.alphacoders.com/799/79.jpg', 'https://avatarfiles.alphacoders.com/114/114197.jpg', 'https://avatarfiles.alphacoders.com/841/84143.png', 'https://avatarfiles.alphacoders.com/583/58365.jpg', 'https://avatarfiles.alphacoders.com/253/25343.jpg']
@@ -55,10 +56,19 @@ function checkMatch(array) {
   const toRemove = document.getElementsByClassName('flipped')
   if (array[0] === array[1]) {
     for (let i = 0; i < toRemove.length; i++) {
+      toRemove[i].push(cardsMatched);
       toRemove[i].style.visibility = 'hidden';
     }
   } else {
     resetCard(toRemove);
+  }
+}
+
+//Function will check array storing matched cards for the correct amount
+//in order to win game
+function gameWin(cardArray) {
+  if (cardArray === 12) {
+    
   }
 }
 
